e603ad29b1355e0801af5439589607a4338aa466
Burko Viktor <victor.burko@avalon.ru>
2013-11-16 15:14:05 +0400

    change setting5

--- practice_5/imgs/imgs/settings.py
+++ practice_5/imgs/imgs/settings.py
@@ -16,12 +16,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(PROJECT_ROOT, 'data.db'),    # Or path to database file if using sqlite3.
-# The following settings are not used with sqlite3:
-#'USER': '',
-#'PASSWORD': '',
-#'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#'PORT': '',                      # Set to empty string for default.
-    }
+		# The following settings are not used with sqlite3:
+		#'USER': '',
+		#'PASSWORD': '',
+		#'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+		#'PORT': '',                      # Set to empty string for default.
+		}
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
